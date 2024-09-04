4b9bd2190d2f31324a1d7c28b595eaeafb9a62ad
unkpell0 <112400860+unkpell0@users.noreply.github.com>
2024-09-04 13:48:46 +0700

    Create departemen.sql

---
+++ departemen.sql
@@ -0,0 +1,12 @@
+INSERT INTO departemen (id_departemen, nama_departemen, lokasi) 
+VALUES 
+('D001', 'Human Resources', 'Jakarta'),
+('D002', 'Finance', 'Surabaya'),
+('D003', 'Marketing', 'Bandung'),
+('D004', 'IT', 'Jakarta'),
+('D005', 'Customer Service', 'Semarang'),
+('D006', 'Production', 'Yogyakarta'),
+('D007', 'Research and Development', 'Malang'),
+('D008', 'Logistics', 'Makassar'),
+('D009', 'Sales', 'Denpasar'),
+('D010', 'Quality Assurance', 'Medan');
